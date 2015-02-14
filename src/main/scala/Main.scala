d97f699c82cba025ed4bbb33a735f773d6b05fcd
saito400 <ken.s.530000@gmail.com>
2015-02-14 16:00:17 +0900

    c

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,22 @@
 
 object Main {
 
-  def main(args: Array[String]): Unit ={
+  def main(args: Array[String]): Unit = {
     println("start")
 
+    val re = add(1,2)
+    println(re)
 
     println("end")
+
+  }
+
+
+  def add(val1: Int, val2: Int): Int = {
+
+
+    val1 + val2
+
 
   }
 
