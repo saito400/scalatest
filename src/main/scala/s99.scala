c3288c6f3e4bcdf27e0f59c9d54059a74a84b472
saito400 <ken.s.530000@gmail.com>
2015-02-22 15:51:19 +0900

    c

--- src/main/scala/s99.scala
+++ src/main/scala/s99.scala
@@ -1,15 +1,10 @@
 
 object s99 extends App {
 
-
-
-
   println(p01(List(1, 1, 2, 3, 5, 8)))
 
   def p01(l: List[Int]): Int ={
-
     l.last
-
   }
 
   def p02[A](list: List[A]):A = list match {
@@ -17,5 +12,11 @@ object s99 extends App {
     case _ :: tail     => p02(tail)
     case _ => throw new NoSuchElementException
   }
+
+  def p03[A](position: Int, list:List[A]) : A = {
+    list(position)
+  }
+
+
 
 }
--- src/test/scala/S99Spec.scala
+++ src/test/scala/S99Spec.scala
@@ -9,10 +9,14 @@ class S99Spec extends FunSpec with Matchers {
     }
 
     it("p02") {
-
       s99.p02(List(1, 1, 2, 3, 5, 8)) should be (5)
-
     }
+
+    it("p03") {
+      s99.p03(2, List(1, 1, 2, 3, 5, 8)) should be (2)
+    }
+
+
 
 
   }
