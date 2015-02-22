4c57750e6d52b0af97aa3e9a75f3da08819ba44f
saito400 <ken.s.530000@gmail.com>
2015-02-22 20:49:28 +0900

    c

--- src/main/scala/Main2.scala
+++ src/main/scala/Main2.scala
@@ -14,12 +14,24 @@ object Main2 extends App {
 
   val m = Map("a" -> Map("aa" -> "bb"))
 
+  println(m)
   println(m("a")("aa"))
 
+  mapTest(m)
 
   def twice(op: Double => Double, x: Double) = op(op(x))
 
+  def mapTest(m: Map[String,Any]) = {
 
+    val mm = m match {
+      case m: Map[String,Map[String,String]] => m("a")("aa")
+      case _ => None
+
+    }
+
+    println(mm)
+
+  }
 
 
 }
