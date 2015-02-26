396ef5f6279419374f108a0545e452bcbc58da51
kenichi.saito@bizreach.co.jp <ken.s.530000@gmail.com>
2015-02-26 14:41:45 +0900

    fixup!  c

--- src/main/scala/Main3.scala
+++ src/main/scala/Main3.scala
@@ -3,13 +3,22 @@ object Main3 extends App {
 
   println("start")
 
-  //(salary.hourly.max,List(1800))
-
-  val map: Map[String, List[Int]] = Map("salary.hourly.average" -> List(1800))
 
 
-  val re = map.get("salary.hourly.average").map(_.head.toLong).getOrElse(0)
 
+  val map: Map[String, List[Any]] = Map("key1" -> List(1800))
+
+
+  f(map)
   println("end")
 
+
+
+  def f(map: Map[String, List[Any]])  = {
+
+    val m = map.asInstanceOf[Map[String, List[String]]]
+
+    val re = m.get("key1").map(_.head.toLong).getOrElse(0)
+
+  }
 }
