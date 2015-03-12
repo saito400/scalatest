d8c4744314bc1761f7451ec52e0f5ccf8f0c11da
saito400 <ken.s.530000@gmail.com>
2015-03-13 07:27:28 +0900

    c

--- src/main/scala/Main6.scala
+++ src/main/scala/Main6.scala
@@ -1,9 +1,6 @@
 
 object Main6 extends App {
 
-
-  
-
   println("start")
 
 //  val v = Seq((1,"val1",1),(1,"val2",2),(1,"val3",3))
@@ -13,26 +10,36 @@ object Main6 extends App {
 
   println(v)
 
-  val v2 = v.map{t => t._1}.toSeq
-
-  println(v2)
-
-  val v3 = v.map{t => t._1}.distinct
-  println(v3)
-
-  val m = v.groupBy(_._1)
-  println(m)
-
-  val m2 = v.map(t => (t._1, t._2)).groupBy(_._1)
-  println(m2)
-
+//  val v2 = v.map{t => t._1}.toSeq
+//  println(v2)
+//
+//  val v3 = v.map{t => t._1}.distinct
+//  println(v3)
+//
+//  val m = v.groupBy(_._1)
+//  println(m)
+//
+//  val m2 = v.map(t => (t._1, t._2)).groupBy(_._1)
+//  println(m2)
+//
   val m3 = v.map(t => (t._1, t._2)).groupBy(_._1).map(x => x._1 -> x._2.distinct)
   println(m3)
 
+//  val m4 = v.map(t => (t._2, t._3)).groupBy(_._1)
+//  println(m4)
+
+  val m5 = v.map(t => (t._2, t._3)).groupBy(_._1).map(x => x._1 -> x._2.distinct)
+  println(m5)
+
+  val m6 = v.groupBy(_._1).map(x => x._1 -> x._2.distinct)
+  println("m6")
+  println(m6)
+  println("m6 foreach")
+  m6.valuesIterator.foreach(println _)
+  println("m6 map")
+  m6.valuesIterator.map(println _)
 
 
   println("end")
-
-  
 
 }
