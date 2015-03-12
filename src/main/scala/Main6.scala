f2df2a18e60c9279780404cee75f520fe6aa6fcc
saito400 <ken.s.530000@gmail.com>
2015-03-12 22:23:12 +0900

    c

--- src/main/scala/Main6.scala
+++ src/main/scala/Main6.scala
@@ -7,7 +7,7 @@ object Main6 extends App {
   println("start")
 
 //  val v = Seq((1,"val1",1),(1,"val2",2),(1,"val3",3))
-  val v = Seq((1,1,1),(1,1,2),(1,1,3),(1,2,1),(1,2,3))
+  val v = Seq((1,1,1),(1,1,2),(1,1,3),(1,2,1),(1,2,3),(2,1,1),(2,1,2))
 
 
 
@@ -19,6 +19,9 @@ object Main6 extends App {
 
   val v3 = v.map{t => t._1}.distinct
   println(v3)
+
+  val m = v.groupBy(_._1)
+  println(m)
 
 
 
