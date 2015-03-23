beb400f9b0790ada736ebe01d8aa60fb9260e27f
saito400 <ken.s.530000@gmail.com>
2015-03-24 07:26:25 +0900

    c

--- src/main/scala/Reg.scala
+++ src/main/scala/Reg.scala
@@ -7,8 +7,26 @@ object Reg extends App {
 
   val reg = "%\\{.+?\\}".r
 
+  val matchedList = reg.findAllIn(target).matchData
+
+  println(matchedList)
+
   for (m <- reg.findAllIn(target).matchData)
     println("Found a match  [%s,%s)".format(m.matched,m.matched.drop(2).dropRight(1)))
+
+  val m2 = for (m <- reg.findAllIn(target).matchData) yield {
+    m.matched.drop(2).dropRight(1)
+  }
+
+  println(m2)
+
+  val m3 = for (m <- reg.findAllIn(target).matchData) yield {
+    m.matched.drop(2).dropRight(1)
+  }
+
+  println(m2.toList)
+
+
 
   println("end")
 
