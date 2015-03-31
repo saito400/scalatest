700918b526de081e8c16d0d6739bc85933104b2e
saito400 <ken.s.530000@gmail.com>
2015-03-31 13:59:35 +0900

    c

--- src/main/scala/Tmp.scala
+++ src/main/scala/Tmp.scala
@@ -11,6 +11,13 @@ object Tmp extends App with TestTrait {
 
   println("aaaaa")
 
-  func1()
+
+  val s = ("header %{param1} %{param2} footer" -> "")
+
+  val p = Seq(("param1" -> "val1"),("param2" -> "val2"))
+
+  val re = p.fold(s)((a,b) => a._1.replaceAll("%\\{" + b._1 + "?\\}",b._2) -> "" )
+
+  println(re._1)
 
 }
