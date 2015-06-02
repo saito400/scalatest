a7f65d7896cd363926164ae3d460fd144a1bc20d
saito400 <ken.s.530000@gmail.com>
2015-06-03 07:33:15 +0900

    commit

--- src/main/scala/FPinScala.scala
+++ src/main/scala/FPinScala.scala
@@ -16,13 +16,16 @@ object FPinScala extends App {
   println(fib(2))
   println(fib(3))
 
-  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = as match {
-
-
-
-
-    false//TODO
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int): Boolean =
+      if (n >= as.length - 1) true
+      else if (ordered(as(n), as(n + 1))) false
+      else go(n + 1)
+    go(0)
   }
+
+  println(isSorted(Array(1,2,3), (i: Int, j: Int) => if(i < j) true else false))
 
 
 
