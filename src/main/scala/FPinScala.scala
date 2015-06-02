73cb34545ade51340ea5e7c402e5ff2857e21776
saito400 <ken.s.530000@gmail.com>
2015-06-02 21:46:52 +0900

    add fib

--- src/main/scala/FPinScala.scala
+++ src/main/scala/FPinScala.scala
@@ -1,8 +1,22 @@
-import scala.annotation.tailrec
 
 object FPinScala extends App {
 
   println("start")
+
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int, prev: Int, cur: Int): Int =
+      if (n == 0) prev
+      else loop(n - 1, cur, prev + cur)
+    loop(n, 0, 1)
+  }
+
+
+  println(fib(0))
+  println(fib(1))
+  println(fib(2))
+  println(fib(3))
+
 
   println("end")
 
