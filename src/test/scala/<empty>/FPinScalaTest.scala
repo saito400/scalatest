25136e406cb0b679957b9e9132bba8bb81b42eff
saito400 <ken.s.530000@gmail.com>
2015-06-06 08:32:17 +0900

    c

--- src/test/scala/<empty>/FPinScalaTest.scala
+++ src/test/scala/<empty>/FPinScalaTest.scala
@@ -7,10 +7,12 @@ class FPinScalaTest extends FunSpec with Matchers {
       FPinScala.fib(0) should be(0)
       FPinScala.fib(1) should be(1)
       FPinScala.fib(2) should be(1)
-
     }
 
-
+    it("isSorted") {
+      FPinScala.isSorted(Array(1,2,3), (i: Int, j: Int) => if(i < j) true else false ) should be (true)
+      FPinScala.isSorted(Array(1,3,2), (i: Int, j: Int) => if(i < j) true else false ) should be (false)
+    }
 
 
   }
