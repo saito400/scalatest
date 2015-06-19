ab9c7bb1554989e51cd23b0448f519c48d9fe41e
saito400 <ken.s.530000@gmail.com>
2015-06-19 16:29:14 +0900

    c

--- src/main/scala/Tmp.scala
+++ src/main/scala/Tmp.scala
@@ -4,7 +4,31 @@ object Tmp extends App {
   println("start")
 
 
+  val re = List(
+    ("Ａ" -> "a")
+    ,("A" -> "a")
+  )
+
+  val s = "ＡａAB"
+
+  println(s)
+
+
+  println(_replace(s, re))
 
 
   println("end")
+
+
+
+  @scala.annotation.tailrec
+  def _replace(s: String, parameters: List[(String, String)]): String = {
+
+    parameters match {
+      case Nil => s
+      case (k, v) :: t => _replace(s.replaceAll(k, v), t)
+    }
+
+
+  }
 }
